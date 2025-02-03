84c96ebde0dd02c3c9670e1d5daef98e5fbd15d3
Novke <novke.brate@gmail.com>
2025-02-03 20:08:07 +0100

    init

---
+++ src/main/java/gfs/webp/ytlerdi/YtLerdiApplication.java
@@ -0,0 +1,13 @@
+package gfs.webp.ytlerdi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class YtLerdiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(YtLerdiApplication.class, args);
+    }
+
+}
---
+++ src/test/java/gfs/webp/ytlerdi/YtLerdiApplicationTests.java
@@ -0,0 +1,13 @@
+package gfs.webp.ytlerdi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class YtLerdiApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
